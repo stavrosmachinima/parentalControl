da5ab7d80307c84f2311775e204da4179e2a1465
Stavros Peppas <S.Peppas@dei.com.gr>
2022-04-15 13:33:48 +0300

    Fixed broken animated button

--- src/main/resources/static/parentsInControl.js
+++ src/main/resources/static/parentsInControl.js
@@ -1,14 +1,6 @@
-$( document ).ready(function() {
-
-  $("#animatebutton").click(function(){
-    const element = document.querySelector('.animatebutton');
-    element.classList.add('animated', 'rotateIn');
-    setTimeout(function() {
-      element.classList.remove('rotateIn');
-    }, 1000);
+  $("#glow").click(function(){
     $('#begin').fadeOut(1500);
     setTimeout(function() {
       $('#menu').fadeIn(2000);
     }, 1500);
     });
-});
